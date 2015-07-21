85bef17fe42d0a10db50bf00c2bbc645e9302ec0
Andrew Wortham <awortham@worthams.net>
2015-07-21 16:26:29 -0400

    admin functions

---
+++ app/assets/javascripts/show_groups.js
@@ -0,0 +1,18 @@
+function show(n) {
+  $.ajax({
+    type: "GET",
+    url:  "/groups/" + n,
+    success: function(response) {
+      document.getElementById("name").innerHTML = "Name: " + response.name;
+      var students = ''
+      response.users.forEach(function(student) {
+        students = students + student + "<br>"
+      });
+      document.getElementById("students").innerHTML = students
+    },
+    error: function(response) {
+
+      console.log(response);
+    }
+  });
+};
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,6 +26,42 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+      @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    Usergroup.where(group_id: @group.id).destroy_all
+    if @group.update_attributes(group_params)
+      students = params[:group][:user_ids]
+      students.pop
+      students.each do |user_id|
+        Usergroup.create(user_id: user_id, group_id: @group.id)
+      end
+      flash[:success] = 'Your edits were saved!'
+      redirect_to groups_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Group.find(params[:id]).destroy
+    redirect_to groups_path
+  end
+
+  def show
+    group = Group.find(params[:id])
+    info = {}
+    info[:name] = group.name
+    info[:users] = []
+    group.users.each do |student|
+      info[:users] << [ student.name]
+    end
+    render json: info
+  end
+
   private
 
   def group_params
