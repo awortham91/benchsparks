a89d90389b829d02b626887bfc6997ecd6bf60c5
Andrew Wortham <awortham@worthams.net>
2015-07-26 01:26:53 -0400

    master fully functional

--- app/assets/javascripts/multiplication_master.js
+++ app/assets/javascripts/multiplication_master.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	if($('div').is('.multiplication_master')){
-
-
+    var seconds = 4;
+		var first = document.getElementById("first_num").innerHTML = " "
+		var second = document.getElementById("second_num").innerHTML = " "
     document.getElementById("counter").onmouseover = function() {
       document.getElementById("ready_text").innerHTML = "Set?";
     }
@@ -13,66 +14,60 @@ $(document).ready(function() {
     document.getElementById("counter").onclick = function() {master()};
 
     function master() {
+			document.getElementById("master_title").style.display = "none";
+			document.getElementById("master_info").style.display = "none";
+			document.getElementById("countdown").innerHTML = 0
       document.getElementById("get_ready2").style.display = "none";
       document.getElementById("countdown_wrapper").style.display = "block";
       document.getElementById("counter").innerHTML = 'Master!';
       document.getElementById('answer_form').className += ' outer_div';
       document.getElementById("answer_form").style.border = "5px solid #510EAB";
       document.getElementById("countdown_wrapper").style.background = "#3849B5";
-      document.getElementById("points").innerHTML = 0;
-      document.getElementById("total_right").innerHTML = 0;
-      document.getElementById("total_wrong").innerHTML = 0;
-      document.getElementById("stats").style.display = "none";
-      toggle_away();
+			document.getElementById("answer_zone").style.display = "block";
+			document.getElementById("info").style.display = "block";
+			first = document.getElementById("first_num").innerHTML = Math.floor((Math.random() * 10) + 1);
+			second = document.getElementById("second_num").innerHTML = Math.floor((Math.random() * 10) + 1);
 
       document.getElementById("counter").disabled = true;
-        var seconds = 61;
-        function tick() {
-          var counter = document.getElementById("countdown");
-          seconds--;
-          counter.innerHTML = (seconds < 10 ? "0" : "") + String(seconds);
-          if( seconds > 0 ) {
-              setTimeout(tick, 1000);
-          } else {
-            var levelData = {
-              "level": level,
-              "score": document.getElementById("points").innerHTML
-            };
-            document.getElementById('answer_form').className -= ' outer_div';
-            document.getElementById("counter").disabled = false;
-            document.getElementById("answer_zone").style.display = "none";
-            document.getElementById("stats").style.display = "block";
-            document.getElementById("answer_form").style.border = "0 solid #EFA43";
-            document.getElementById("countdown_wrapper").style.background = '#04839B';
-            document.getElementById("level_select").style.visibility = "visible";
-            document.getElementById("counter").innerHTML = "Start the timer!";
-            $.ajax({
-              type: "POST",
-               url: "/timedpractices",
-              contentType: "application/json",
-              dataType: "json",
-              data: JSON.stringify(levelData),
-               success: function(response) {
-                 console.log(response);
-               },
-               error: function(response) {
-                 console.log(response)
-               }
-            });
+      seconds = 5;
+      function tick() {
+        var counter = document.getElementById("countdown");
+        seconds--;
+        if( seconds > 0 ) {
+          setTimeout(tick, 1000);
+        } else {
+          var levelData = {
+            "level": "4",
+            "score": document.getElementById("countdown").innerHTML
           };
+          document.getElementById('answer_form').className -= ' outer_div';
+          document.getElementById("counter").disabled = false;
+          document.getElementById("master_title").style.display = "block";
+					document.getElementById("master_info").style.display = "block";
+					document.getElementById("answer_zone").style.display = "none";
+          document.getElementById("answer_form").style.border = "0 solid #EFA43";
+          document.getElementById("countdown_wrapper").style.background = '#04839B';
+          document.getElementById("counter").innerHTML = "Start the timer!";
+          $.ajax({
+            type: "POST",
+            url: "/timedpractices",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(levelData),
+            success: function(response) {
+              console.log(response);
+            },
+            error: function(response) {
+              console.log(response)
+            }
+          });
         };
-        tick();
-
-    };
-
-    function toggle_away() {
-      document.getElementById("answer_zone").style.display = "block";
-      document.getElementById("info").style.display = "block";
-    };
+      };
+			tick();
+		};
 
     $(function() {
       $(".sub_button").click(function() {
-        var level = document.getElementById("range").innerHTML;
         if ($('#answer_area').val() != '') {
           var user_id = document.getElementById("user").innerHTML;
           var answer=$('#answer_area').val();
@@ -80,78 +75,62 @@ $(document).ready(function() {
             "body": first + " * " + second,
             "answer": answer
           };
-          $.ajax({
-            type: 'POST',
-            url:  "/userquestions",
-            contentType: "application/json",
-            dataType: "json",
-            data: JSON.stringify(data),
-            success: function(response) {
+				};
+        $.ajax({
+          type: 'POST',
+          url:  "/userquestions",
+          contentType: "application/json",
+          dataType: "json",
+          data: JSON.stringify(data),
+          success: function(response) {
+            $.ajax({
+              type: "PATCH",
+              url: "/questions/" + user_id,
+              success: function(response) {
+                console.log(response);
+              },
+              error: function(response) {
+                console.log(response)
+              }
+            });
+            if (response.statusText === 'correct') {
               $.ajax({
                 type: "PATCH",
-                 url: "/questions/" + user_id,
-                 success: function(response) {
-                   console.log(response);
-                 },
-                 error: function(response) {
-                   console.log(response)
-                 }
+                url: "/questions/" + user_id + "/correct",
+                success: function(response) {
+                  console.log(response);
+                },
+                error: function(response) {
+                  console.log(response)
+                }
               });
-              console.log(response)
-              if (level === '1') {
-                first = document.getElementById("first_num").innerHTML = easy()
-                second = document.getElementById("second_num").innerHTML = easy()
-              } else if (level === '2') {
-                first = document.getElementById("first_num").innerHTML = medium()
-                second = document.getElementById("second_num").innerHTML = medium()
-              }  else {
-                first = document.getElementById("first_num").innerHTML = hard()
-                second = document.getElementById("second_num").innerHTML = hard()
-              }
-              var totals = document.getElementById("points").innerHTML;
-              var totals = (parseInt(totals));
-              var correct = document.getElementById("total_right").innerHTML;
-              var correct = (parseInt(correct));
-              var incorrect = document.getElementById("total_wrong").innerHTML;
-              var incorrect = (parseInt(incorrect));
-              if (response.statusText === 'correct') {
-                var totals = totals += 1
-                var correct = correct += 1
-                $.ajax({
-                  type: "PATCH",
-                   url: "/questions/" + user_id + "/correct",
-                   success: function(response) {
-                     console.log(response);
-                   },
-                   error: function(response) {
-                     console.log(response)
-                   }
-                });
-              } else {
-                var totals = totals -= 1
-                var incorrect = incorrect += 1
-                $.ajax({
-                  type: "PATCH",
-                   url: "/questions/" + user_id + "/incorrect",
-                   success: function(response) {
-                     console.log(response);
-                   },
-                   error: function(response) {
-                     console.log(response)
-                   }
-                });
-              };
-              document.getElementById("points").innerHTML = totals;
-              document.getElementById("total_right").innerHTML = correct;
-              document.getElementById("total_wrong").innerHTML = incorrect;
-            },
-            error: function(response) {
-              console.log(response)
-            }
-          });
-          $("#answer_area").val('');
-        };
-        return false;
+							var totals = (parseInt(document.getElementById("countdown").innerHTML));
+							totals = totals += 1;
+							document.getElementById("countdown").innerHTML = totals;
+							first = document.getElementById("first_num").innerHTML = Math.floor((Math.random() * 10) + 1);
+	            second = document.getElementById("second_num").innerHTML = Math.floor((Math.random() * 10) + 1);
+							seconds = 3
+            } else {
+							first = document.getElementById("first_num").innerHTML = "-";
+							second = document.getElementById("second_num").innerHTML = "-";
+              $.ajax({
+                type: "PATCH",
+                url: "/questions/" + user_id + "/incorrect",
+                success: function(response) {
+                  console.log(response);
+                },
+                error: function(response) {
+                  console.log(response)
+                }
+              });
+            };
+          },
+          error: function(response) {
+            console.log(response)
+          }
+        });
+        $("#answer_area").val('');
+				return false;
       });
     });
 
